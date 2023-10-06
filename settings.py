7122628ddef69a2f48e6ddd01f7da2344d09a52c
Palemoky <palemoky@gmail.com>
2023-10-06 10:46:50 +0800

    完成第12章内容

---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings:
+    """存储游戏《外星人入侵》中所有设置的类"""
+
+    def __init__(self):
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # 飞船移速设置
+        self.ship_speed = 1.75
+
+        # 子弹设置
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+class Ship:
+    """管理飞船的类"""
+
+    def __init__(self, ai_game):
+        """初始化飞船并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 每艘飞船都在屏幕底部的中央
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # 将飞船的 x 坐标转换为浮点数，便于后续移动时的坐标计算
+        self.x = float(self.rect.x)
+
+        # 移动标志
+        self.moving_left = False
+        self.moving_right = False
+
+    def update(self):
+        if self.moving_left and self.rect.left > 0:
+            """ 
+            注意此处不可直接计算更新 self.rect.x！
+            因为 pygame 中该坐标以整数表示，而移速是浮点数，计算更新会导致精度丢失，左右移速不一致的问题。
+            先将浮点数计算结果暂存在 self.x 中再赋值给 self.rect.x，则可避免浮点数精度丢失的问题。
+            """
+            self.x -= self.settings.ship_speed
+        elif self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        self.rect.x = self.x
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
